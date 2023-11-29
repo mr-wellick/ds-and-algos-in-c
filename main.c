0c96c2ec6f23a67ac08d030f2a3b507a474b8d65
mr-wellick <lorenz3192@gmail.com>
2023-11-28 18:44:39 -0800

    bytes & bits basics for int and float types

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
+// directives (#include, for example): editing commands that modify the program prior to compilation
+// 	- intended for the preprocessor
+// headers (<stdio.h>): each contains information about some part of the standard library
 #include <stdio.h>
 
+// functions:  named blocks of executable code, of which main in an example
+// 	- a series of statements that have been grouped together and given a name
 int main(void) {
-	printf("To C, or not to C: that is the question.\n");
-	return 0;
+	// statements: commands to be performed when the program is run
+	printf("To C, or not to C: that is the question.\n"); // function call
+	return 0; // return statement
 }
