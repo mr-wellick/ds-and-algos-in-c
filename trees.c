b4bf8e25f703a497b702140af8435c729d576123
mr-wellick <lorenz3192@gmail.com>
2023-11-29 01:27:57 -0800

    practice binary trees
    	- Print tree items: pre order traversal
    	- Insert nodes into binary trees
    	- Free memory: post order traversal

--- trees.c
+++ trees.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// create tree node
+// factory method: different types of trees with different root nodes need to be created
+//typedef struct TreeFactory {
+//	// function pointer with a return type of Node* with params: int value
+//	Node* (*createRootNode)(int value); 
+//} TreeFactory;
+
+// Example on how to use this typedef
+// = { createSimpleRootNode }: This is the initializer list, and it's used to initialize the members of the TreeFactory structure.
+//TreeFactory simpleTreeFactory = { createSimpleRootNode };
+//Node* root = simpleTreeFactory.createRootNode(10);
+
 typedef struct Node {
 	int value;
 	struct Node* left;
 	struct Node* right;
 } Node;
-
-
-// factory method: different types of trees with different root nodes need to be created
-typedef struct TreeFactory {
-	// function pointer with a return type of Node* with params: int value
-	Node* (*createRootNode)(int value); 
-} TreeFactory;
 
 Node* createSimpleRootNode(int value){
 	Node* rootNode = (Node*)malloc(sizeof(Node)); // type cast pointer return by malloc (void*) to Node*
@@ -24,18 +27,56 @@ Node* createSimpleRootNode(int value){
 	return rootNode;
 }
 
-// add nodes to tree
-// void addNode()
+Node* addNode(Node* curr,  int value){
+	if(curr == NULL){
+		curr = createSimpleRootNode(value);
+	} else if(value < curr->value) {
+		curr->left = addNode(curr->left, value);
+	} else if(value > curr->value) {
+		curr->right = addNode(curr->right, value);
+	}	
+
+	return curr;
+}
+
+void demolish(Node* curr){
+	if(curr == NULL) {
+		return;
+	}
+
+	demolish(curr->left);
+	demolish(curr->right);
+	// post-order traversal
+	printf("post-order traversal: %d\n", curr->value);
+	free(curr);
+}
+
+void printTree(Node* curr){
+	if(curr == NULL){
+		return;
+	}
+
+	// pre-order traversal
+	printf("pre-order traversal: %d\n", curr->value);
+	printTree(curr->left);
+	printTree(curr->right);
+}
 
 
 int main(void){
-	// = { createSimpleRootNode }: This is the initializer list, and it's used to initialize the members of the TreeFactory structure.
-	TreeFactory simpleTreeFactory = { createSimpleRootNode };
-	Node* rootOne = simpleTreeFactory.createRootNode(10);
+	Node *root = NULL;
+	//       10
+	//     /    \
+	//    5      15
+	//   / \     / \
+	//  N  N    N   N
+	root = addNode(root, 10);
+	root = addNode(root, 5);
+	root = addNode(root, 15);
 
-	// print value of root node
-	printf("Simple root %d\n", rootOne->value);
-	free(rootOne);
+	printTree(root);
+	printf("\n");
+	demolish(root);
 
 	return 0;
 }
