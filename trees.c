37b773b0bd2183eb6b08cff9a82a71fb0e6968ec
mr-wellick <lorenz3192@gmail.com>
2023-12-04 16:50:03 -0800

    successfully compiled source files

--- trees.c
+++ trees.c
@@ -1,5 +1,4 @@
 #include <stdbool.h>
-#include "trees.h"
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -87,34 +86,3 @@ void demolish(Node *curr) {
   free(curr);
 }
 
-//int main(void) {
-//  // create BST and add values
-//  Node *root = createNode(10);
-//  /*      10
-//        /    \
-//       5      15
-//      / \     / \
-//    -5  N    N   N
-//    /\
-//   N N                */
-//  insert(root, 5);
-//  insert(root, 5);
-//  insert(root, 15);
-//  insert(root, -5);
-//
-//  // find a value
-//  int needle = -5;
-//  printf("\n");
-//  printf("Was %d found?\n", needle);
-//  printf("True: 1 or False: 0\n");
-//  printf("Result: %d\n", find(root, needle));
-//  printf("\n");
-//
-//  // perform operations before freeing up the tree lol
-//  printTree(root);
-//  printf("\n");
-//  demolish(root);
-//  printf("\n");
-//
-//  return 0;
-//}
