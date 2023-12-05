70a60b11a5179aab0ddfff09fa766f3040457c28
mr-wellick <lorenz3192@gmail.com>
2023-12-04 17:27:39 -0800

    add comments on run time of binary tree search insertion

--- trees.h
+++ trees.h
@@ -7,14 +7,20 @@ typedef struct Node Node;
 
 Node *createNode(int value);
 
+/* Big Oh of BST Insertion
+ *
+ * - It's Log2(N) because first we have to use a binary search to find where to insert our node.
+ *   Recall, binary search is Log2(N).
+ *
+ * */
 void insert(Node *curr, int value);
 
 /* Big Oh of BST Search
  *
  * - In the average BST with N values, how many steps are required to find our
- * value? ==> Log2(N) steps.
+ *   value? ==> Log2(N) steps.
  * - In the worst case BST with N values, how many steps are required to find
- * our value? ==> N steps.
+ *   our value? ==> N steps.
  *
  * */
 bool find(Node *curr, int value);
