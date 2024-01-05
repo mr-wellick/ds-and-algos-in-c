437d3e0a15a545c73a8a24e41c7a14b90a0c5176
mr-wellick <lorenz3192@gmail.com>
2024-01-05 13:12:50 -0800

    add size macro and init adjacency list for E2 string

--- src/graphs/tests/test.c
+++ src/graphs/tests/test.c
@@ -2,6 +2,9 @@
 #include "../graphs.h"
 #include <stdlib.h>
 
+#define SEMITONES 22
+#define SIZE ((int)(sizeof(E2_adjacencyList) / sizeof(E2_adjacencyList[0])))
+
 // Optional: Any setup code that needs to run before each test
 void setUp(void) {}
 
@@ -20,9 +23,18 @@ void test_create_edge(void) {
   TEST_ASSERT_NULL(edge);
 }
 
+void test_create_graph(void) {
+  Edge E2_adjacencyList[SEMITONES] = {};
+
+  for (int i = 0; i < SIZE; i++) {
+    printf("test %d", E2_adjacencyList[i].to);
+  }
+}
+
 int main(void) {
   UNITY_BEGIN();
   RUN_TEST(test_create_edge);
+  RUN_TEST(test_create_graph);
   UNITY_END();
 
   return 0;
